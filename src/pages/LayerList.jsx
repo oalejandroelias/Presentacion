49a0a6a325a179fa226bb84ee392473dce8bb2ab
oalejandroelias <oalejandroelias@gmail.com>
2024-06-19 15:50:30 -0300

    Listado de capas desde Geoserver.

--- src/pages/LayerList.jsx
+++ src/pages/LayerList.jsx
@@ -2,45 +2,56 @@ import { useEffect, useState, useMemo } from "react";
 import { XMLParser } from "fast-xml-parser";
 import styled from "styled-components";
 import Table from "../components/DataTable";
+import axios from "axios";
 
 const LayerList = () => {
   const clickhandler = (name) => console.log("delete", name);
   const [capas, setCapas] = useState([]);
-  const [baseGeoUrl, setbaseGeoUrl] = useState("http://giscopade.neuquen.gov.ar/geoserver/ows")
+
+  var base_url = window.location.protocol + "//" + window.location.host;
+
+  //const [baseGeoUrl, setbaseGeoUrl] = useState("http://giscopade.neuquen.gov.ar/geoserver/wms?request=getCapabilities")
+  const [baseGeoUrl, setbaseGeoUrl] = useState(base_url + "/geoserver/wms?request=getCapabilities")
 
   //const baseGeoUrl = "http://sigepen.neuquen.gov.ar:8080/geoserver/ows?service=wms&version=1.3.0&request=GetCapabilities";
   //const baseGeoUrl = "http://aicsig.neuquen.gov.ar:8080/geoserver/ows?service=wms&version=1.3.0&request=GetCapabilities";
 
   useEffect(() => {
-    const getXMLResponse = async () => {
-      const resp = await fetch(baseGeoUrl+"?service=wms&version=1.3.0&request=GetCapabilities")
-        .then((response) => response.text())
-        .then((textResponse) => {
-          const parser = new XMLParser();
-          let obj = parser.parse(textResponse);
-          setCapas(obj.WMS_Capabilities.Capability.Layer.Layer);
-          // console.log("AAA")
-          // console.log(obj.WMS_Capabilities.Capability)
-          // console.table(obj.WMS_Capabilities.Capability.Layer.Layer[0])
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    };
-
     // const getXMLResponse = async () => {
-    //   const resp = axios.get(baseGeoUrl).then((response) => {
-
-    //     const parser = new XMLParser();
-    //     let obj = parser.parse(response.data);
-    //     console.log(obj)
-    //     setCapas(obj.WMS_Capabilities.Capability.Layer.Layer);
-
-    //   })
+    //   const resp = await fetch(baseGeoUrl+"?service=wms&version=1.3.0&request=GetCapabilities")
+    //     .then((response) => response.text())
+    //     .then((textResponse) => {
+    //       const parser = new XMLParser();
+    //       let obj = parser.parse(textResponse);
+    //       setCapas(obj.WMS_Capabilities.Capability.Layer.Layer);
+    //       // console.log("AAA")
+    //       // console.log(obj.WMS_Capabilities.Capability)
+    //       // console.table(obj.WMS_Capabilities.Capability.Layer.Layer[0])
+    //     })
     //     .catch((error) => {
     //       console.log(error);
     //     });
     // };
+
+    const getXMLResponse = async () => {
+      const resp = axios.get(baseGeoUrl, {
+        headers: {
+          'Content-Type': 'application/xml'
+        }
+      }).then((response) => {
+
+        const parser = new XMLParser();
+        let obj = parser.parse(response.data);
+        console.log("entro")
+        console.log(obj)
+        console.log(obj.WMS_Capabilities.Capability.Layer.Layer)
+        setCapas(obj.WMS_Capabilities.Capability.Layer.Layer);
+
+      })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
 
     getXMLResponse();
   }, []);
@@ -149,7 +160,7 @@ const LayerList = () => {
 
   return (
     <div className="lg:w-5/6 w-full mx-auto overflow-auto mt-20">
-      <Table data={columns} baseGeoUrl={baseGeoUrl} click={clickhandler} />
+      <Table data={capas} baseGeoUrl={baseGeoUrl} click={clickhandler} />
     </div>
   );
 };
